5f93b2f6076dbd9046fb1a0d67f88b1d17736f4b
hiroki <hiroki@d8c447d1-404f-0410-b513-df65077e64da>
2007-03-13 22:14:02 +0000

    Added TestSuite class for TELS service classes

---
+++ webapp/trunk/src/test/java/org/telscenter/sail/webapp/service/impl/AllTests.java
@@ -0,0 +1,24 @@
+/**
+ * TODO: replace with TELS PORTAL copyright information
+ */
+package org.telscenter.sail.webapp.service.impl;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author Hiroki Terashima
+ *
+ * @version $Id: $
+ *
+ */
+public class AllTests {
+ 
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for org.telscenter.sail.webapp.service.impl");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(UserServiceImplTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+}
