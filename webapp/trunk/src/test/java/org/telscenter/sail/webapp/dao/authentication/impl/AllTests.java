2dba0dfdede3734c00cee930a663d4dc85042d76
hiroki <hiroki@d8c447d1-404f-0410-b513-df65077e64da>
2007-03-13 22:44:29 +0000

    Added AllTests ot org.telscenter.sail.webapp.dao.authentication.impl package

---
+++ webapp/trunk/src/test/java/org/telscenter/sail/webapp/dao/authentication/impl/AllTests.java
@@ -0,0 +1,25 @@
+/**
+ * TODO: replace with TELS PORTAL copyright information
+ */
+package org.telscenter.sail.webapp.dao.authentication.impl;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author Hiroki Terashima
+ *
+ * @version $Id: $
+ *
+ */
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(
+				"Test for org.telscenter.sail.webapp.dao.authentication.impl");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(HibernateStudentUserDetailsDaoTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+}
