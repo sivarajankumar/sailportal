fd0b8aedca97545179355bfadc6879d128e801ab
hiroki <hiroki@d8c447d1-404f-0410-b513-df65077e64da>
2007-03-17 07:17:52 +0000

    Added copyright info

--- webapp/trunk/src/main/java/net/sf/sail/webapp/presentation/validators/UserDetailsValidator.java
+++ webapp/trunk/src/main/java/net/sf/sail/webapp/presentation/validators/UserDetailsValidator.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2006 Encore Research Group, University of Toronto
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package net.sf.sail.webapp.presentation.validators;
 
 import net.sf.sail.webapp.domain.authentication.MutableUserDetails;
@@ -7,6 +24,13 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+/**
+ * The validator for the signup page.
+ * 
+ * @author Laurel Williams
+ * 
+ * @version $Id$
+ */
 public class UserDetailsValidator implements Validator {
 
   public static final int MAX_USERNAME_LENGTH = 50;
