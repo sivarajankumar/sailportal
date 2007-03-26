6d2a631ec94961d951bb3ca1b66127984aaf347f
hiroki <hiroki@d8c447d1-404f-0410-b513-df65077e64da>
2007-03-26 07:03:00 +0000

    Added Toronto's copyright info, @author, and @version

--- webapp/trunk/src/main/java/net/sf/sail/webapp/domain/User.java
+++ webapp/trunk/src/main/java/net/sf/sail/webapp/domain/User.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2006 Encore Research Group, University of Toronto
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package net.sf.sail.webapp.domain;
 
 import java.io.Serializable;
@@ -5,6 +22,11 @@ import java.io.Serializable;
 import net.sf.sail.webapp.domain.authentication.MutableUserDetails;
 import net.sf.sail.webapp.domain.sds.SdsUser;
 
+/**
+ * @author Cynick Young
+ * @author Laurel Williams
+ * @version $Id$
+ */
 public interface User extends Serializable {
 
 	/**
