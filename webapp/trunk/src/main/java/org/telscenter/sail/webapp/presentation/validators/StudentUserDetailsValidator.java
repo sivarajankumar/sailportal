fa67c5fd4265f91c7678b9daab3a90cc606a786e
hiroki <hiroki@d8c447d1-404f-0410-b513-df65077e64da>
2007-04-09 19:45:10 +0000

    StudentUserDetailsValidator no longer works with MutableUserDetails; it works with StudentUserDetails

--- webapp/trunk/src/main/java/org/telscenter/sail/webapp/presentation/validators/StudentUserDetailsValidator.java
+++ webapp/trunk/src/main/java/org/telscenter/sail/webapp/presentation/validators/StudentUserDetailsValidator.java
@@ -22,13 +22,12 @@
  */
 package org.telscenter.sail.webapp.presentation.validators;
 
-import net.sf.sail.webapp.domain.authentication.MutableUserDetails;
+
 import net.sf.sail.webapp.presentation.validators.UserDetailsValidator;
 
-import org.apache.commons.lang.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
-import org.springframework.validation.Validator;
+import org.telscenter.sail.webapp.domain.authentication.impl.StudentUserDetails;
 
 /**
  * Validator for the TELS Student User Details upon registering
@@ -38,33 +37,45 @@ import org.springframework.validation.Validator;
  * @version $Id$
  *
  */
-public class StudentUserDetailsValidator extends UserDetailsValidator implements Validator {
+public class StudentUserDetailsValidator extends UserDetailsValidator {
 
 	/**
 	 * @see org.springframework.validation.Validator#supports(java.lang.Class)
 	 */
 	public boolean supports(Class clazz) {
-		return super.supports(clazz);
+		return StudentUserDetails.class.isAssignableFrom(clazz);
 	}
 
 	/**
 	 * @see org.springframework.validation.Validator#validate(java.lang.Object, org.springframework.validation.Errors)
 	 */
 	public void validate(Object userDetailsIn, Errors errors) {
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstname",
+		"error.firstname-not-specified");
+
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastname",
+		"error.lastname-not-specified");
+		
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "gender",
+		"error.gender-not-specified");
+
+		/*
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "birthdate",
+		"error.birthdate-not-specified");
+		 */
+		
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password",
 		"error.password-not-specified");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username",
-		"error.username-not-specified");
-		if (errors.getFieldErrorCount("username") > 0) {
-			return;
-		}
-		MutableUserDetails userDetails = (MutableUserDetails) userDetailsIn;
-		if (!StringUtils.isAlphanumeric(userDetails.getUsername())) {
-			errors.rejectValue("username", "error.illegal-characters");
-		}
-		if (userDetails.getUsername().length() > MAX_USERNAME_LENGTH) {
-			errors.rejectValue("username", "error.too-long");
-		}
+//		if (errors.getFieldErrorCount("username") > 0) {
+//			return;
+//		}
+		StudentUserDetails userDetails = (StudentUserDetails) userDetailsIn;
+//		if (!StringUtils.isAlphanumeric(userDetails.getUsername())) {
+//			errors.rejectValue("username", "error.illegal-characters");
+//		}
+//		if (userDetails.getUsername().length() > MAX_USERNAME_LENGTH) {
+//			errors.rejectValue("username", "error.too-long");
+//		}
 		if (errors.hasErrors())
 			userDetails.setPassword("");
 	}
