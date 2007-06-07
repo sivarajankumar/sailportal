11601bd506c2485b9c0d52963cb159f225486cbc
hiroki <hiroki@d8c447d1-404f-0410-b513-df65077e64da>
2007-06-07 15:43:06 +0000

    Added Equals() and HashCode() methods to PersistentGroup

--- webapp/trunk/src/main/java/net/sf/sail/webapp/domain/group/impl/PersistentGroup.java
+++ webapp/trunk/src/main/java/net/sf/sail/webapp/domain/group/impl/PersistentGroup.java
@@ -180,4 +180,47 @@ public class PersistentGroup implements Group {
 		this.parent = parent;
 	}
 
+	/**
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int PRIME = 31;
+		int result = 1;
+		result = PRIME * result + ((members == null) ? 0 : members.hashCode());
+		result = PRIME * result + ((name == null) ? 0 : name.hashCode());
+		result = PRIME * result + ((parent == null) ? 0 : parent.hashCode());
+		return result;
+	}
+
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final PersistentGroup other = (PersistentGroup) obj;
+		if (members == null) {
+			if (other.members != null)
+				return false;
+		} else if (!members.equals(other.members))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (parent == null) {
+			if (other.parent != null)
+				return false;
+		} else if (!parent.equals(other.parent))
+			return false;
+		return true;
+	}
+
 }
