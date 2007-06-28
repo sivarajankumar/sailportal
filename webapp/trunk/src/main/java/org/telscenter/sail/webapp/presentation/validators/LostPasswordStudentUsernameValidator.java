8b0aef32d40cf1b95e4c11ee1752865aedb59373
tony <tony@d8c447d1-404f-0410-b513-df65077e64da>
2007-06-28 19:35:09 +0000

    the validator

--- webapp/trunk/src/main/java/org/telscenter/sail/webapp/presentation/validators/LostPasswordStudentUsernameValidator.java
+++ webapp/trunk/src/main/java/org/telscenter/sail/webapp/presentation/validators/LostPasswordStudentUsernameValidator.java
@@ -25,12 +25,13 @@ package org.telscenter.sail.webapp.presentation.validators;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.validation.Errors;
 import org.telscenter.sail.webapp.domain.authentication.impl.StudentUserDetails;
+import org.telscenter.sail.webapp.domain.authentication.impl.TeacherUserDetails;
 
 /**
  * validates the username of the student.
  * 
  * @author Anthony Perritano
- * @version $Id$
+ * @version $$Id$$
  */
 public class LostPasswordStudentUsernameValidator extends StudentUserDetailsValidator {
 
