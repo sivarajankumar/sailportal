d04be7fb4bb7ddc1fa0cf984d6e6b5a5f5e8d967
tony <tony@d8c447d1-404f-0410-b513-df65077e64da>
2007-07-12 00:29:47 +0000

    for lostpassword wizard

---
+++ webapp/trunk/src/main/java/org/telscenter/sail/webapp/presentation/web/controllers/LostPasswordStudentReminderWizardController.java
@@ -0,0 +1,216 @@
+/**
+ * Copyright (c) 2007 Regents of the University of California (Regents). Created
+ * by TELS, Graduate School of Education, University of California at Berkeley.
+ *
+ * This software is distributed under the GNU Lesser General Public License, v2.
+ *
+ * Permission is hereby granted, without written agreement and without license
+ * or royalty fees, to use, copy, modify, and distribute this software and its
+ * documentation for any purpose, provided that the above copyright notice and
+ * the following two paragraphs appear in all copies of this software.
+ *
+ * REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE SOFTWAREAND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+ * HEREUNDER IS PROVIDED "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE
+ * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ * IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+ * SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+ * ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ * REGENTS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.telscenter.sail.webapp.presentation.web.controllers;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.sail.webapp.domain.User;
+import net.sf.sail.webapp.domain.authentication.MutableUserDetails;
+import net.sf.sail.webapp.service.UserService;
+import net.sf.sail.webapp.service.curnit.CurnitNotFoundException;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.validation.BindException;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractWizardFormController;
+import org.springframework.web.servlet.view.RedirectView;
+import org.telscenter.sail.webapp.domain.authentication.impl.StudentUserDetails;
+import org.telscenter.sail.webapp.domain.impl.ReminderParameters;
+import org.telscenter.sail.webapp.domain.impl.RunParameters;
+
+/**
+ * Controller for the wizard to "Remind the student of their password"
+ * 
+ * The default getTargetPage() method is used to find out which page to navigate to, so
+ * the controller looks for a request parameter starting with "_target" and ending with
+ * a number (e.g. "_target1"). The jsp pages should provide these parameters.
+ *
+ * General method invocation flow (when user clicks on "prev" and "next"): 
+ *  1) onBind
+ *  2) onBindAndValidate
+ *  3) validatePage
+ *  4) referenceData
+ * Note that on user's first visit to the first page of the wizard, only referenceData will be
+ * invoked, and steps 1-4 are bypassed.
+ *
+ * @author Anthony Perritano
+ * @version $Id$
+ */
+
+public class LostPasswordStudentReminderWizardController extends
+		AbstractWizardFormController {
+
+	private static final String ACCOUNT_QUESTION = "accountQuestion";
+	private static final String USERNAME = "username";
+	protected UserService userService = null;
+	private User user;
+	
+	/**
+	 * Constructor
+	 *  - Specify the pages in the wizard
+	 *  - Specify the command name
+	 */
+	public LostPasswordStudentReminderWizardController() {
+		setBindOnNewForm(true);
+		setPages(new String[]{"lostpasswordstudentpasswordwizardreminder1", "lostpasswordstudentpasswordwizardreminder2","lostpasswordstudentpasswordwizardreminder3"});
+		setSessionForm(true);
+	}
+	
+	/**
+	 * @see org.springframework.web.servlet.mvc.BaseCommandController#onBind(javax.servlet.http.HttpServletRequest, java.lang.Object, org.springframework.validation.BindException)
+	 */
+	@Override
+	protected void onBind(HttpServletRequest request,
+			Object command, BindException errors) throws Exception {
+		// TODO AP: implement me
+	    super.onBind(request, command, errors);
+	}
+	
+	/**
+	 * This method is called after the onBind and onBindAndValidate method. It acts 
+	 * in the same way as the validator
+	 * 
+	 * @see org.springframework.web.servlet.mvc.AbstractWizardFormController#validatePage(java.lang.Object, org.springframework.validation.Errors, int)
+	 */
+	@Override
+	protected void validatePage(Object command, Errors errors, int page) {
+		// TODO AP: implement me
+	    super.validatePage(command, errors, page);
+	}
+	
+	/**
+	 * This method is called right before the view is rendered to the user
+	 * 
+	 * @see org.springframework.web.servlet.mvc.AbstractWizardFormController#referenceData(javax.servlet.http.HttpServletRequest, int)
+	 */
+	@Override
+	protected Map<String, Object> referenceData(HttpServletRequest request, 
+			Object command, Errors errors, int page) {
+		
+		ReminderParameters reminderParameters = (ReminderParameters) command;
+
+		Map<String, Object> model = new HashMap<String, Object>();
+		switch(page) {
+		case 0:
+			break;
+		case 1:
+			String username = reminderParameters.get(ReminderParameters.USERNAME);
+			
+			try {
+
+				username = StringUtils.trimToNull(username);
+				if (username != null) {
+					user = userService.retrieveUserByUsername(username);
+					StudentUserDetails userDetails = (StudentUserDetails) user.getUserDetails();
+					
+					model.put(USERNAME, username);
+					model.put(ACCOUNT_QUESTION, userDetails.getAccountQuestion());
+					
+					reminderParameters.setAccountQuestion(userDetails.getAccountQuestion());
+					reminderParameters.setAccountAnswer(userDetails.getAccountAnswer());
+				} else {
+					
+			        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "accountQuestion","error.no-accountquestion");
+					
+					
+				}// if
+
+			} catch (EmptyResultDataAccessException e) {
+				e.printStackTrace();
+			}
+			break;
+		case 2:
+			String submittedAccountAnswer = reminderParameters.getSubmittedAccountAnswer();
+			
+			System.out.println("submittedAccountAnswer: " + submittedAccountAnswer);
+			
+			
+			break;
+	
+		case 3:
+		case 4:
+		case 5:
+		default:
+			break;
+		}
+
+		return model;
+	}
+	
+	/**
+	 * changes the password
+	 * 
+	 * This method is called if there is a submit that validates and contains the "_finish"
+	 * request parameter.
+	 * 
+	 * @see org.springframework.web.servlet.mvc.AbstractWizardFormController#processFinish(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, java.lang.Object, org.springframework.validation.BindException)
+	 */
+	@Override
+	protected ModelAndView processFinish(HttpServletRequest request,
+			HttpServletResponse response, Object command, BindException errors)
+			throws Exception {
+		
+		ReminderParameters runParameters = (ReminderParameters) command;
+
+		 ModelAndView modelAndView = new ModelAndView();
+
+	     return modelAndView;
+	}
+	
+	/**
+	 * This method is called if there is a submit that contains the "_cancel"
+	 * request parameter.
+	 * 
+	 * @see org.springframework.web.servlet.mvc.AbstractWizardFormController#processCancel(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, java.lang.Object, org.springframework.validation.BindException)
+	 */
+	@Override
+	protected ModelAndView processCancel(HttpServletRequest request,
+			HttpServletResponse response, Object command, BindException errors) {
+		return new ModelAndView(new RedirectView("lostpasswordmain.html"));
+	}
+	
+	/**
+	 * Sets the userDetailsService object.
+	 * 
+	 * @param userDetailsService
+	 */
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
+	
+	
+
+}
---
+++ webapp/trunk/src/main/webapp/WEB-INF/jsp/tels/lostpasswordstudentpasswordwizardreminder3.jsp
@@ -0,0 +1,55 @@
+`<%@ include file="include.jsp"%>
+<!--
+  * Copyright (c) 2006 Encore Research Group, University of Toronto
+  * 
+  * This library is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU Lesser General Public
+  * License as published by the Free Software Foundation; either
+  * version 2.1 of the License, or (at your option) any later version.
+  *
+  * This library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  * Lesser General Public License for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public
+  * License along with this library; if not, write to the Free Software
+  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+-->
+
+<!-- $Id: login.jsp 341 2007-04-26 22:58:44Z hiroki $ -->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "XHTML1-s.dtd" >
+<html xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<link href="<spring:theme code="stylesheet"/>" media="screen" rel="stylesheet"
+  type="text/css" />
+<script src="./javascript/tels/prototype.js" type="text/javascript" ></script>
+<script src="./javascript/tels/scriptaculous.js" type="text/javascript" ></script>
+<title><spring:message code="teacher.setup-project-run-step-one" /></title>
+
+</head>
+<body>
+<%@ include file="teacher/header.jsp"%>
+<div id="navigation" class="center north2 widthAdj4">
+<ul class="bigFont1">
+<li style="background-color:#FFFFFF;"> <a href="#"> <spring:message code="banner.home" /> </a> </li>
+<li class="bgColorLightBlue"  class="border"> <a href="#"><spring:message code="banner.projects" /> </a> </li>
+<li style="background-color:#FFFFFF;"> <a href="#"> <spring:message code="banner.management" /> </a> </li>
+<li style="background-color:#FFFFFF;"> <a href="#"> <spring:message code="banner.community" /> </a> </li>
+<li style="background-color:#FFFFFF;"> <a href="#"> <spring:message code="banner.help" /> </a> </li>
+</ul>
+</div>
+
+
+<p class="bigFont1 north0">step 3</p> 
+ 
+<form id="submittedAccountAnswer" method="post" class="center north0" commandName="reminderParameters" >
+
+<input type="submit" name="_target2" value="<spring:message code="navigate.back" />">
+<input type="submit" name="_target3" value="<spring:message code="navigate.next" />">
+<input type="submit" name="_cancel" value="<spring:message code="navigate.cancel" />">
+</form>
+</body>
+</html>
