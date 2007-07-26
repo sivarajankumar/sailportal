19dd5f9c7663b171427e396b04be378cd5d9a932
hiroki <hiroki@d8c447d1-404f-0410-b513-df65077e64da>
2007-07-26 21:25:16 +0000

    TELSP-91 changed suffix of same teacher usernames from letters to numbers.

--- webapp/trunk/src/main/java/org/telscenter/sail/webapp/domain/authentication/impl/TeacherUserDetails.java
+++ webapp/trunk/src/main/java/org/telscenter/sail/webapp/domain/authentication/impl/TeacherUserDetails.java
@@ -232,8 +232,8 @@ public class TeacherUserDetails extends PersistentUserDetails implements
 	 * @see org.telscenter.sail.webapp.domain.authenticationMutableUserDetails.getUsernameSuffixes()
 	 */
 	public String[] getUsernameSuffixes() {
-		return new String[] {"", "a", "b", "c", "d", "e", "f", "g", "h",
-	            "i", "j", "k", "l", "m", "n", "o", "p"};
+		return new String[] {"", "1", "2", "3", "4", "5", "6", "7", "8",
+	            "9", "10", "11", "12", "13", "14", "15", "16"};
 	}
 	
 	public Integer getNumberOfLogins() {
