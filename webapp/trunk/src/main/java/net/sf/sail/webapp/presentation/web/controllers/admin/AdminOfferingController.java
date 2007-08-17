96af80d4164d20ceb597287ae439f496212b8021
laurel <laurel@d8c447d1-404f-0410-b513-df65077e64da>
2007-08-17 16:07:53 +0000

    revised security authentication for adminoffering page

--- webapp/trunk/src/main/java/net/sf/sail/webapp/presentation/web/controllers/admin/AdminOfferingController.java
+++ webapp/trunk/src/main/java/net/sf/sail/webapp/presentation/web/controllers/admin/AdminOfferingController.java
@@ -15,7 +15,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-package net.sf.sail.webapp.presentation.web.controllers.offerings;
+package net.sf.sail.webapp.presentation.web.controllers.admin;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
