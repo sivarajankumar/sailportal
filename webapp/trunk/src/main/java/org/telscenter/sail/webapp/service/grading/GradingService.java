fcc8dc2a9c801ea7b210bed81dfd988ccdbb77a5
hiroki <hiroki@d8c447d1-404f-0410-b513-df65077e64da>
2007-09-11 20:53:15 +0000

    Added GradeWorkAggregate, GradeWorkByStepAggregate, GradeWorkByWorkgroupAggregate domain object interfaces and their implementing classes, as well as GradingService and GradingServiceImpl classes.

---
+++ webapp/trunk/src/main/java/org/telscenter/sail/webapp/service/grading/GradingService.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2007 Regents of the University of California (Regents). Created
+ * by TELS, Graduate School of Education, University of California at Berkeley.
+ *
+ * This software is distributed under the GNU Lesser General Public License, v2.
+ *
+ * Permission is hereby granted, without written agreement and without license
+ * or royalty fees, to use, copy, modify, and distribute this software and its
+ * documentation for any purpose, provided that the above copyright notice and
+ * the following two paragraphs appear in all copies of this software.
+ *
+ * REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE SOFTWAREAND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+ * HEREUNDER IS PROVIDED "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE
+ * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ * IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+ * SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+ * ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ * REGENTS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.telscenter.sail.webapp.service.grading;
+
+import java.util.List;
+
+import net.sf.sail.webapp.dao.ObjectNotFoundException;
+import net.sf.sail.webapp.domain.annotation.AnnotationBundle;
+
+import org.telscenter.pas.emf.pas.ECurnitmap;
+import org.telscenter.pas.emf.pas.EStep;
+import org.telscenter.sail.webapp.domain.grading.GradeWorkByStepAggregate;
+import org.telscenter.sail.webapp.domain.grading.GradeWorkByWorkgroupAggregate;
+
+/**
+ * Services for WISE teachers for grading student work. Grading involves
+ * working with <code>Curnitmap</code>, <code>AnnotationBundle</code>,
+ * <code>SessionBundle</code> objects and services that are available for
+ * those objects.
+ * 
+ * @author Hiroki Terashima
+ * @version $ Id: $
+ */
+public interface GradingService {
+	
+	/**
+	 * Returns a <code>ECurnitMap</code> representation of the 
+	 * <code>Curnit</code> that is associated with a <code>Run</code>
+	 * with the specified runId.
+	 * 
+	 * If the runId is not associated with any existing <code>Run</code>, 
+	 * <code>ObjectNotFoundException</code> is thrown.
+	 * 
+	 * @param runId id of the run that the teacher wants to grade
+	 * @return <code>List</code> of grade-able steps
+	 * @throws <code>ObjectNotFoundException</code> when the provided runId
+	 *     does not key to an existing <code>Run</code>
+	 */
+	public ECurnitmap getCurnitmap(Long runId) throws ObjectNotFoundException;
+	
+	/**
+	 * Returns an aggregate object to allow WISE teachers to grade student
+	 * work for a particular <code>Step</code> of a particular 
+	 * <code>Project</code> that is being used in a <code>Run</code> indicated
+	 * by the runId.
+	 * 
+	 * All of the workgroups' work is retrieved along with their respective
+	 * <code>AnnotationBundle</code> for the particular step.
+	 * 
+	 * @param runId id of the run that the teacher wants to grade
+	 * @param step particular step that the teacher wants to grade
+	 * @return <code>GradeWorkAggregate</code> containing all of the students'
+	 *     work for the specified step.
+	 */
+	public GradeWorkByStepAggregate getGradeWorkByStepAggregate(Long runId, EStep step);
+	
+	/**
+	 * Returns an aggregate object to allow WISE teachers to grade student work
+	 * of a particular <code>Workgroup</code> of a particular 
+	 * <code>Project</code> that is being used in a <code>Run</code> indicated
+	 * by the runId.
+	 * 
+	 * All of the specified workgroup's work is retrieved along with its
+	 * <code>AnnotationBundle</code> for the entire project.
+	 * 
+	 * @param runId id of the run that the teacher wants to grade
+	 * @param workgroupId particular workgroup that the teacher wants to grade
+	 * @return <code>gradeWorkAggregate</code> containing all of the workgroup's
+	 *     work for the entire project.
+	 */
+	public GradeWorkByWorkgroupAggregate getGradeWorkByStepAggregate(Long runId, Long workgroupId);
+	
+	/**
+	 * Saves the grades and comments
+	 * 
+	 * @param annotationBundles all of the annotationbundles to save
+	 */
+	public void saveGrades(List<AnnotationBundle> annotationBundles);
+}
---
+++ webapp/trunk/src/main/java/org/telscenter/sail/webapp/service/grading/impl/GradingServiceImpl.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2007 Regents of the University of California (Regents). Created
+ * by TELS, Graduate School of Education, University of California at Berkeley.
+ *
+ * This software is distributed under the GNU Lesser General Public License, v2.
+ *
+ * Permission is hereby granted, without written agreement and without license
+ * or royalty fees, to use, copy, modify, and distribute this software and its
+ * documentation for any purpose, provided that the above copyright notice and
+ * the following two paragraphs appear in all copies of this software.
+ *
+ * REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE SOFTWAREAND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+ * HEREUNDER IS PROVIDED "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE
+ * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ * IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+ * SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+ * ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ * REGENTS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.telscenter.sail.webapp.service.grading.impl;
+
+import java.util.List;
+
+import net.sf.sail.webapp.dao.ObjectNotFoundException;
+import net.sf.sail.webapp.domain.annotation.AnnotationBundle;
+
+import org.telscenter.pas.emf.pas.ECurnitmap;
+import org.telscenter.pas.emf.pas.EStep;
+import org.telscenter.sail.webapp.domain.grading.GradeWorkByStepAggregate;
+import org.telscenter.sail.webapp.domain.grading.GradeWorkByWorkgroupAggregate;
+import org.telscenter.sail.webapp.service.grading.GradingService;
+
+/**
+ * @author Hiroki Terashima
+ * @version $ Id: $
+ */
+public class GradingServiceImpl implements GradingService {
+
+	/**
+	 * @see org.telscenter.sail.webapp.service.grading.GradingService#getCurnitmap(java.lang.Long)
+	 */
+	public ECurnitmap getCurnitmap(Long runId) throws ObjectNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see org.telscenter.sail.webapp.service.grading.GradingService#getGradeWorkByStepAggregate(java.lang.Long, org.telscenter.pas.emf.pas.EStep)
+	 */
+	public GradeWorkByStepAggregate getGradeWorkByStepAggregate(Long runId,
+			EStep step) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see org.telscenter.sail.webapp.service.grading.GradingService#getGradeWorkByStepAggregate(java.lang.Long, java.lang.Long)
+	 */
+	public GradeWorkByWorkgroupAggregate getGradeWorkByStepAggregate(
+			Long runId, Long workgroupId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see org.telscenter.sail.webapp.service.grading.GradingService#saveGrades(java.util.List)
+	 */
+	public void saveGrades(List<AnnotationBundle> annotationBundles) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
