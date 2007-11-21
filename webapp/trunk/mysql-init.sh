f2a39d7477e07d2e7084d537d6d6b1667f0d1478
david <david@d8c447d1-404f-0410-b513-df65077e64da>
2007-11-21 07:06:03 +0000

    - update ant build.xml and other configuration for mysql

---
+++ webapp/trunk/mysql-init.sh
@@ -0,0 +1,2 @@
+cp ~/.m2/repository/org/telscenter/external/org/acegisecurity/acegi-security/1.0.4-SNAPSHOT/acegi-security-1.0.4-SNAPSHOT.jar ~/.m2/repository/org/acegisecurity/acegi-security/1.0.4/acegi-security-1.0.4.jar 
+mysql -u root -p < mysql-init.sql
