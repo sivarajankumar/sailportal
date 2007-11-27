c3118ad29c004f377689a9975820990a3ef15192
tony <tony@d8c447d1-404f-0410-b513-df65077e64da>
2007-11-27 20:23:44 +0000

    part of the DWR work of breaking up the validator

--- webapp/trunk/src/main/java/net/sf/sail/webapp/presentation/validators/ChangePasswordParametersValidator.java
+++ webapp/trunk/src/main/java/net/sf/sail/webapp/presentation/validators/ChangePasswordParametersValidator.java
@@ -22,20 +22,23 @@
  */
 package net.sf.sail.webapp.presentation.validators;
 
+import java.lang.reflect.Method;
+
 import net.sf.sail.webapp.domain.impl.ChangePasswordParameters;
 
 import org.apache.commons.lang.StringUtils;
+import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
-import org.telscenter.sail.webapp.domain.impl.*;
+import org.telscenter.sail.webapp.domain.impl.BatchStudentChangePasswordParameters;
+import org.telscenter.sail.webapp.domain.impl.Passwords;
 
 /**
  * Validator for student's ChangePasswordParameters
  * 
  * @author Patrick Lawler
- * @author Sally Ahn
- * $Id:$
+ * @author Sally Ahn $Id:$
  */
 public class ChangePasswordParametersValidator implements Validator {
 
@@ -46,47 +49,154 @@ public class ChangePasswordParametersValidator implements Validator {
 	 */
 	@SuppressWarnings("unchecked")
 	public boolean supports(Class clazz) {
-		return ChangePasswordParameters.class.isAssignableFrom(clazz) || BatchStudentChangePasswordParameters.class.isAssignableFrom(clazz);
+		return ChangePasswordParameters.class.isAssignableFrom(clazz)
+				|| BatchStudentChangePasswordParameters.class
+						.isAssignableFrom(clazz);
 	}
 
 	/**
-	 * @see org.springframework.validation.Validator#validate(java.lang.Object, org.springframework.validation.Errors)
+	 * @see org.springframework.validation.Validator#validate(java.lang.Object,
+	 *      org.springframework.validation.Errors)
 	 */
 	public void validate(Object paramsIn, Errors errors) {
 		ChangePasswordParameters params = (ChangePasswordParameters) paramsIn;
 
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "passwd1",
-				"error.no-password");
+		validatePasswd1(errors,params);
 		
-		if (errors.getErrorCount() != 0) {
+		if( errors.getErrorCount() != 0 )
 			return;
-		}
-
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "passwd2",
-				"error.no-password");
 		
-		if (errors.getErrorCount() != 0) {
+		validatePasswd2(errors,params);
+		
+		if( errors.getErrorCount() != 0 )
 			return;
-		}
-
-		if (params.getPasswd1().length() > MAX_PASSWORD_LENGTH || 
-				params.getPasswd2().length() > MAX_PASSWORD_LENGTH) {
-			errors.rejectValue("passwd1", "error.password-too-long");
-			return;
-		}
-
-		if (!StringUtils.isAlphanumeric(params.getPasswd1()) || 
-				!StringUtils.isAlphanumeric(params.getPasswd2())) {
-			errors.rejectValue("passwd1", "error.password-illegal-characters");
-			return;
-		}
-
+		
+		
 		Passwords passwords = new Passwords(params.getPasswd1(), params.getPasswd2());
+		validatePasswordsMatch(errors, passwords, params);
+	}
+	
+	public void validatePasswd1(Errors errors, ChangePasswordParameters params) {
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "passwd1",
+		"error.no-password");
+		
+		if( errors.getErrorCount() != 0 )
+			return;
+		this.validatePasswordAlphaNumeric(errors, params.getPasswd1());
+	}
+	
+	public void validatePasswd2(Errors errors, ChangePasswordParameters params) {
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "passwd2",
+		"error.no-password");
+		
+		if( errors.getErrorCount() != 0 )
+			return;
+		
+		this.validatePasswordAlphaNumeric(errors, params.getPasswd2());
+	}
 
+	private void validatePasswordsMatch(Errors errors, Passwords passwords, ChangePasswordParameters params) {
+		
+		//lengths match
+		validatePasswordLength(errors, params);
+		
 		if (!passwords.match()) {
 			errors.rejectValue("passwd1", "error.passwords-mismatch");
 		}
-
 	}
+
+	public void validatePasswordAlphaNumeric(Errors errors,
+			String passwd) {
+		if (!StringUtils.isAlphanumeric(passwd))
+			errors.rejectValue("passwd1", "error.password-illegal-characters");
+		
+	}
+
+	public void validatePasswordLength(Errors errors,
+			ChangePasswordParameters params) {
+		if (params.getPasswd1().length() > MAX_PASSWORD_LENGTH
+				|| params.getPasswd2().length() > MAX_PASSWORD_LENGTH) {
+			errors.rejectValue("passwd1", "error.password-too-long");
+		}
+	}
+
+	
+	public String getInputFieldValidationMessage(String formInputId, String formInputValue) {
+		return "this is a message error";
+	}
+	/**
+	 * Get the validation message for an individual input field of a model
+	 * object.
+	 * 
+	 * @param modelObject
+	 *            The object to validate against.
+	 * @param formInputId
+	 *            The id attribute of the form input field.
+	 * @param formInputValue
+	 *            The input value to be validated.
+	 * @return The validation message.
+	 */
+//	public String getInputFieldValidationMessage(String formInputId,
+//			String formInputValue) {
+//
+//		String validationMessage = "";
+//
+//		try {
+//			Object formBackingObject = new ChangePasswordParameters();
+//			Errors errors = new BindException(formBackingObject, "command");
+//
+//			formInputId = formInputId.split("\\.")[1]; // Ignore the preceding
+//			// "command." portion of
+//			// the id
+//			String capitalizedFormInputId = StringUtils.capitalize(formInputId);
+//
+//			// Invoke the set[formInputId] method on the Account instance
+//			String accountMethodName = "set" + capitalizedFormInputId;
+//			Class setterArgs[] = new Class[] { String.class };
+//			Method accountMethod = formBackingObject.getClass().getMethod(
+//					accountMethodName, setterArgs);
+//			accountMethod.invoke(formBackingObject,
+//					new Object[] { formInputValue });
+//
+//			// Invoke the validate[formInputId] method of the AccountValidator
+//			// instance
+//			String validationMethodName = "validate" + capitalizedFormInputId;
+//			Class validationArgs[] = new Class[] { String.class, Errors.class };
+//			Method validationMethod = getClass().getMethod(
+//					validationMethodName, validationArgs);
+//			validationMethod.invoke(this,
+//					new Object[] { formInputValue, errors });
+//
+//			validationMessage = getValidationMessage(errors, formInputId);
+//		} catch (Exception e) {
+//			// Handle appropriately for your application
+//			System.out.println("New code exception: " + e);
+//		}
+//
+//		return validationMessage;
+//	}
+//
+//	/**
+//	 * Get the FieldError validation message from the underlying MessageSource
+//	 * for the given fieldName.
+//	 * 
+//	 * @param errors
+//	 *            The validation errors.
+//	 * @param fieldName
+//	 *            The fieldName to retrieve the error message from.
+//	 * @return The validation message or an empty String.
+//	 */
+//	protected String getValidationMessage(Errors errors, String fieldName) {
+//		String message = "";
+//
+//		FieldError fieldError = errors.getFieldError(fieldName);
+//
+//		if (fieldError != null) {
+//			message = messageSource.getMessage(fieldError.getCode(), null,
+//					"This field is invalid", Locale.ENGLISH);
+//		}
+//
+//		return message;
+//	}
 
 }
