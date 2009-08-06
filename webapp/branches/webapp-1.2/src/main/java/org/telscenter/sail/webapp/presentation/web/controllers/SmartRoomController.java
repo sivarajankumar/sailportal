bd613271d049610d174c980a1cc3ee645f1fa02b
somebodyiknow <somebodyiknow@d8c447d1-404f-0410-b513-df65077e64da>
2009-08-06 11:56:27 +0000

    Adding more services

--- webapp/branches/webapp-1.2/src/main/java/org/telscenter/sail/webapp/presentation/web/controllers/SmartRoomController.java
+++ webapp/branches/webapp-1.2/src/main/java/org/telscenter/sail/webapp/presentation/web/controllers/SmartRoomController.java
@@ -28,11 +28,10 @@ import org.telscenter.sail.webapp.service.student.StudentService;
 
 public class SmartRoomController  extends AbstractController {
 	
-//	private UserService userService;
-//
 	private static final String USERID = "userId";
 	private static final String RUNID = "runId";
 	private static final String OFFERINGID = "offeringId";
+	private static final String GROUPID = "groupId";
 	
 	private WorkgroupService workgroupService;
 	private StudentService studentService;
@@ -50,33 +49,41 @@ public class SmartRoomController  extends AbstractController {
 		String action = request.getParameter("action");
 		if (action != null) {
 			if (action.equals("getUserInfo")) {
-				// if getUserInfo is requested, return xmlString instead in the response
+				// Returns the USER information of the specific USER_ID as an XML string 
 				// for see the result http://localhost:8080/webapp/smartroom/smartroom.html?action=getUserInfo&userId=3
 				return handleGetUserInfo(request, response);
-			}if (action.equals("getRunsList")) {
-				// if getRunList is requested, return xmlString instead in the response
+			}if (action.equals("getRunsList")) {//======================= does not work properly
+				// Returns the RUNs information of the specific USER as an XML string 
 				// for see the result http://localhost:8080/webapp/smartroom/smartroom.html?action=getRunsList&userId=3
 				return handleGetRunsList(request, response);
 			}if (action.equals("getOwnersList")) {
-				// if getOwnersList is requested, return xmlString instead in the response
-				// for see the result http://localhost:8080/webapp/smartroom/smartroom.html?action=getOwnersList&runId=1
+				// Returns the OWNERs information of the specific RUN as an XML String 
+				// for see the result http://localhost:8080/webapp/smartroom/smartroom.html?action=getOwnersList&runId=2
 				return handleGetOwnersList(request, response);
 			}if (action.equals("getRunInfo")) {
-				// if getRunInfo is requested, return xmlString instead in the response
+				// Returns the RUN information of the specific RUN_ID as an XML String 
 				// for see the result http://localhost:8080/webapp/smartroom/smartroom.html?action=getRunInfo&runId=1
 				return handleGetRunInfo(request, response);
-				///***************************************************************************************
 			}if (action.equals("getGroups")) {
-				// for see the result http://localhost:8080/webapp/smartroom/smartroom.html?action=getGroups&userId=3
+				//retrieves all Groups by specific offeringId
+				// for see the result http://localhost:8080/webapp/smartroom/smartroom.html?action=getGroups&offeringId=2
 				return handleGetGroups(request, response);
-			}if (action.equals("getRuns")) {
-				// if getRuns is requested, return xmlString instead in the response
-				// for see the result http://localhost:8080/webapp/smartroom/smartroom.html?action=getRuns&offeringId=1
-				return handleGetRunsByOfferingId(request, response);
+			}if (action.equals("getMembersGroup")) {
+				// retrieves all members(users) in a Group by specific groupId
+				// for see the result http://localhost:8080/webapp/smartroom/smartroom.html?action=getMembersGroup&groupId=6
+				return handleGetMembersGroup(request, response);
+			}if (action.equals("getWorkgroups")) {
+				//returns the workgroups of the specific OFFERING ID as an XML String 
+				// for see the result http://localhost:8080/webapp/smartroom/smartroom.html?action=getgetWorkgroups&offeringId=2
+				return handleGetWorksgroups(request, response);
 			}if (action.equals("getOfferings")) {
-				// if getOfferings is requested, return xmlString instead in the response
+				// returns the offerings(runs)information 
 				// for see the result http://localhost:8080/webapp/smartroom/smartroom.html?action=getOfferings
 				return handleGetOfferings(request, response);
+			}if (action.equals("getWorkgroups")) {//++++++++++++++++++++++++++++++++++++++ this is for test
+				// returns all information about workgroups this is for test 
+				// for see the result http://localhost:8080/webapp/smartroom/smartroom.html?action=getWorkgroups
+				return handleGetWorkgroups(request, response);
 			} else {
 				// shouldn't get here
 				throw new RuntimeException("should not get here");
@@ -176,7 +183,7 @@ public class SmartRoomController  extends AbstractController {
 		return null;
 	}
 	/**
-	 * Returns the RUNs ID of the specific OFFERING ID as an XML String 
+	 * //retrieve all Groups by specific offeringId as an XML String
 	 * @param request
 	 * @param response
 	 * @return
@@ -186,30 +193,76 @@ public class SmartRoomController  extends AbstractController {
 	private ModelAndView handleGetGroups(HttpServletRequest request,
 			HttpServletResponse response) throws IOException, ObjectNotFoundException {
 
-		//Long userId = Long.parseLong(request.getParameter(USERID));
-		List<Group> groupList = this.groupService.getGroups();
-		
- 		String groupXML = "<groups>";
-		for(Iterator<Group> iter = groupList.iterator(); iter.hasNext();){
-			
-			Group group = iter.next();
-			groupXML += "<name>" + group.getName() + "</name>";
-			groupXML += "<id>" + group.getId() + "</id>";
+		Long offeringId = Long.parseLong(request.getParameter(OFFERINGID));
+		Boolean no_groups = true;
+		String groupXML = "<groups>";
+		List<Workgroup> workgroupsList = this.workgroupService.getWorkgroupList();
+		for(Iterator<Workgroup> iter = workgroupsList.iterator(); iter.hasNext();){
+			Workgroup workgroup = iter.next();
+			Offering offering = workgroup.getOffering();
+			if (offeringId.longValue() == offering.getId()){
+				Group group = workgroup.getGroup();
+				groupXML += "<group>";
+				groupXML += "<id>" + group.getId().toString() + "</id>";
+				groupXML += "<name>" + group.getName() + "</name>";
+				groupXML += "</group>";
+				no_groups = false; 
+			}
 		}
+		if (no_groups)
+			groupXML += "null";
 		groupXML += "</groups>";
-		
 		setResponse(response, groupXML);
 		return null;
 	}
 	/**
-	 * Returns the RUNs ID of the specific OFFERING ID as an XML String 
+	 * Returns the Members(users) of a Group by specific group ID as an XML String 
 	 * @param request
 	 * @param response
 	 * @return
 	 * @throws IOException 
 	 * @throws ObjectNotFoundException
 	 */
-	private ModelAndView handleGetRunsByOfferingId(HttpServletRequest request,
+	private ModelAndView handleGetMembersGroup(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ObjectNotFoundException {
+
+		Long groupId = Long.parseLong(request.getParameter(GROUPID));
+		Boolean members_group_is_null = true;
+		String membersGroupXML = "<membersGroup>";
+		List<Workgroup> workgroupsList = this.workgroupService.getWorkgroupList();
+		for(Iterator<Workgroup> iter = workgroupsList.iterator(); iter.hasNext();){
+			Workgroup workgroup = iter.next();
+			Group group = workgroup.getGroup();
+			if (groupId.longValue() == group.getId()){
+				Set<User> groupMembersSet = group.getMembers();
+				//workgroupXML += "<groupMembers>";
+				for(Iterator<User> i = groupMembersSet.iterator(); i.hasNext();){
+					User user = i.next();
+					membersGroupXML += "<memberGroup>";
+					membersGroupXML += "<id>" + user.getUserDetails().getId().toString() + "</id>";
+					membersGroupXML += "<name>" + user.getUserDetails().getUsername().toString() + "</name>";
+					//workgroupXML += "<email>" + user.getUserDetails().getEmailAddress() + "</email>";
+					membersGroupXML += "</memberGroup>";
+					members_group_is_null = false;
+				}
+			}
+		}
+		if (members_group_is_null)
+			membersGroupXML += "null";
+		membersGroupXML += "</membersGroup>";
+		
+		setResponse(response, membersGroupXML);
+		return null;
+	}
+	/**
+	 * Returns the Workgroups of the specific OFFERING ID as an XML String 
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws IOException 
+	 * @throws ObjectNotFoundException
+	 */
+	private ModelAndView handleGetWorksgroups(HttpServletRequest request,
 			HttpServletResponse response) throws IOException, ObjectNotFoundException {
 
 		Long offeringId = Long.parseLong(request.getParameter(OFFERINGID));
@@ -220,14 +273,15 @@ public class SmartRoomController  extends AbstractController {
 			
 			Workgroup workgroup = iter.next();
 			workgroupXML += "<id>" + workgroup.getId() + "</id>";
-//			workgroupXML += workgroup.
+			//workgroupXML += "<sds_name>" + workgroup.getSdsWorkgroup().getName() + "</sds_name>";
 		}
 		workgroupXML += "</workgrpups>";
 		
 		setResponse(response, workgroupXML);
 		return null;
-	}	/**
-	 * Returns the RUN information of the specific RUN_ID as an XML String 
+	}
+	/**
+	 * Returns the offering information 
 	 * is paused or messages that teacher wants to send to students.
 	 * @param request
 	 * @param response
@@ -248,6 +302,60 @@ public class SmartRoomController  extends AbstractController {
 		return null;
 	}
 	/**
+	 * Returns the RUN information of the specific RUN_ID as an XML String 
+	 * is paused or messages that teacher wants to send to students.
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws IOException 
+	 * @throws ObjectNotFoundException 
+	 */
+	private ModelAndView handleGetWorkgroups(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ObjectNotFoundException {
+
+		List<Workgroup> workgroupsList = this.workgroupService.getWorkgroupList() ;
+		String workgroupXML = "<workgroups>";
+		for(Iterator<Workgroup> iter = workgroupsList.iterator(); iter.hasNext();){
+			workgroupXML += "<workgroup>";
+			Workgroup workgroup = iter.next();
+			workgroupXML += "<id_workgroup>" + workgroup.getId().toString() + "</id_workgroup>";
+			Offering offering = workgroup.getOffering();
+			workgroupXML += "<offeringId_workgroup>" + offering.getId().toString() + "</offeringId_workgroup>";
+			Group group = workgroup.getGroup();
+			workgroupXML += "<group>";
+			workgroupXML += "<id_group>" + group.getId().toString() + "</id_group>";
+			workgroupXML += "<name_group>" + group.getName() + "</name_group>";
+			Set<User> groupMembersSet = group.getMembers();
+			workgroupXML += "<groupMembers>";
+			for(Iterator<User> i = groupMembersSet.iterator(); i.hasNext();){
+				User user = i.next();
+				workgroupXML += "<groupMember>";
+				workgroupXML += "<id_groupMember>" + user.getUserDetails().getId().toString() + "</id_groupMember>";
+				workgroupXML += "<name_groupMember>" + user.getUserDetails().getUsername().toString() + "</name_groupMember>";
+				//workgroupXML += "<email>" + user.getUserDetails().getEmailAddress() + "</email>";
+				workgroupXML += "</groupMember>";
+			}
+			workgroupXML += "</groupMembers>";
+			workgroupXML += "</group>";
+
+			Set<User> workgroupMemberSet = workgroup.getMembers();
+			workgroupXML += "<workgroupMembers>";
+			for(Iterator<User> i = groupMembersSet.iterator(); i.hasNext();){
+				User user = i.next();
+				workgroupXML += "<member>";
+				workgroupXML += "<id_member>" + user.getUserDetails().getId().toString() + "</id_member>";
+				workgroupXML += "<name_member>" + user.getUserDetails().getUsername().toString() + "</name_member>";
+				//workgroupXML += "<email>" + user.getUserDetails().getEmailAddress() + "</email>";
+				workgroupXML += "</member>";
+			}
+			workgroupXML += "</workgroupMembers>";
+			workgroupXML += "</workgroup>";
+		}
+		workgroupXML += "</workgroups>";
+		setResponse(response, workgroupXML);
+		return null;
+	}
+	/**
 	 * Returns the RunInfoString XML containing some information about run
 	 * @param run
 	 * @return
@@ -255,8 +363,9 @@ public class SmartRoomController  extends AbstractController {
 	private String makeRunInfoXML(Run run){
 
 		String runInfoXML = "<runInfo>";
-		runInfoXML += "<name>" + run.getName() + "</name>";
 		runInfoXML += "<id>" + run.getId()+ "</id>";
+		runInfoXML += "<name>" + run.getName() + "</name>";
+		runInfoXML += "<runCode>" + run.getRuncode()+ "</runCode>";
 		runInfoXML += "<startTime>" + run.getStarttime().toString() + "</startTime>";
 		if (run.isEnded())
 			runInfoXML += "<endTime>" + run.getEndtime().toString() + "</endTime>";
@@ -293,9 +402,9 @@ public class SmartRoomController  extends AbstractController {
 		offeringXML += "</offering>";
 		return offeringXML;
 	}
-//	public void setWorkgroupService(WorkgroupService workgroupService) {
-//		this.workgroupService = workgroupService;
-//	}
+	public void setWorkgroupService(WorkgroupService workgroupService) {
+		this.workgroupService = workgroupService;
+	}
 //
 //	public void setStudentService(StudentService studentService) {
 //		this.studentService = studentService;
